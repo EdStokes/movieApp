1c925a041da02fd9017de1c2eb62b9c7f16aa17c
Ed Stokes <edstokes96@gmail.com>
2023-07-02 17:18:52 -0500

    added API request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+// API key: 38c34efd
+
+const API_URL = 'http://www.omdbapi.com?apikey=38c34efd';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman')
+    }, []);
     return (
         <h1>App</h1>
     );
